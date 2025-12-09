fb6bdccb0a472741815b064e6db5eadc9d0bfcb9
ribuka <rtaguchi.tte@gmail.com>
2025-12-09 20:33:33 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/humble61mx/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9b, 0xbf, 0x4a, 0x89, 0x3c, 0xa9, 0x2e, 0x5e}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
